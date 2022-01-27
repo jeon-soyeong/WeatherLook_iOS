beeec69b4b9a133424aaed7ee412d3fdf63a566f
전소영 <jsu3417@gmail.com>
2022-01-27 19:16:26 +0900

    Initial Commit

---
+++ WeatherLook_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherLook_iOS
+//
+//  Created by 전소영 on 2022/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherLook_iOSUITests/WeatherLook_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherLook_iOSUITestsLaunchTests.swift
+//  WeatherLook_iOSUITests
+//
+//  Created by 전소영 on 2022/01/27.
+//
+
+import XCTest
+
+class WeatherLook_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
