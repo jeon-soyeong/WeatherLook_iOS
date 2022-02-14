8e6e28d6fb15c57459b6aae3c8883872c8f61d56
전소영 <jsu3417@gmail.com>
2022-02-15 01:58:14 +0900

    [#3] UIColor+Extension 추가

---
+++ WeatherLook_iOS/WeatherLook_iOS/Util/Extension/UIColor+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extension.swift
+//  WeatherLook_iOS
+//
+//  Created by 전소영 on 2022/02/15.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let mainBlue = #colorLiteral(red: 0.2239006013, green: 0.5520019531, blue: 1, alpha: 1)
+}
