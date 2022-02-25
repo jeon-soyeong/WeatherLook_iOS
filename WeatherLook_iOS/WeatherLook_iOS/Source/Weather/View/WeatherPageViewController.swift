88c785a3c8bf1a08e4a418c1605c2342e93fc1a2
전소영 <jsu3417@gmail.com>
2022-02-25 21:01:50 +0900

    [#5] UIPageViewControllerDataSource 추가

--- WeatherLook_iOS/WeatherLook_iOS/Source/Weather/View/WeatherPageViewController.swift
+++ WeatherLook_iOS/WeatherLook_iOS/Source/Weather/View/WeatherPageViewController.swift
@@ -9,9 +9,74 @@ import UIKit
 
 class WeatherPageViewController: UIPageViewController {
     weak var coordinator: WeatherCoordinator?
+    private var weatherViewControllers: [WeatherViewController] = []
+    var pageIndex: Int = 0
+    var weatherViewModel = WeatherViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        dataSource = self
+        delegate = self
+        
+        setupWeatherViewControllers()
+        setupCurrentWeatherViewController()
+    }
+    
+    private func setupWeatherViewControllers() {
+        //FIXME: weatherViewModel로 변경
+        for i in 0..<5 { //weatherViewModel.weatherData.count {
+            if let weatherViewController = createWeatherViewController(at: i) as? WeatherViewController {
+                weatherViewControllers.append(weatherViewController)
+            }
+        }
+    }
+    
+    private func createWeatherViewController(at index: Int) -> UIViewController {
+        let weatherViewController = WeatherViewController()
+        //FIXME: weatherViewModel로 변경
+//        weatherViewController.location = locationList[index]
+//        weatherViewController.index = index
+        
+        return weatherViewController
+    }
+    
+    private func setupCurrentWeatherViewController() {
+        let currentWeatherViewController = weatherViewControllers[pageIndex]
+        setViewControllers([currentWeatherViewController], direction: .forward, animated: false, completion: nil)
     }
 }
+
+// MARK: UIPageViewControllerDataSource
+extension WeatherPageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let weatherViewController = viewController as? WeatherViewController, let currentIndex = weatherViewControllers.firstIndex(of: weatherViewController) else {
+                return nil
+        }
+        
+        let previousIndex = currentIndex - 1
+        
+        if previousIndex < 0 {
+            return weatherViewControllers.last
+        } else {
+            return weatherViewControllers[previousIndex]
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let weatherViewController = viewController as? WeatherViewController, let currentIndex = weatherViewControllers.firstIndex(of: weatherViewController) else {
+                return nil
+        }
+        
+        let nextIndex = currentIndex + 1
+      
+        if nextIndex >= weatherViewControllers.count {
+            return weatherViewControllers.first
+        } else {
+            return weatherViewControllers[nextIndex]
+        }
+    }
+}
+
+// MARK: UIPageViewControllerDelegate
+extension WeatherPageViewController: UIPageViewControllerDelegate { }
