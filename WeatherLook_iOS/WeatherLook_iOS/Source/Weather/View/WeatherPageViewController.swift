e973d75a7ae9f17e7c0eedf13369b153000a83e1
전소영 <jsu3417@gmail.com>
2022-02-25 21:19:25 +0900

    [#5] WeatherPageViewController horizontal scroll 설정 추가

--- WeatherLook_iOS/WeatherLook_iOS/Source/Weather/View/WeatherPageViewController.swift
+++ WeatherLook_iOS/WeatherLook_iOS/Source/Weather/View/WeatherPageViewController.swift
@@ -13,6 +13,14 @@ class WeatherPageViewController: UIPageViewController {
     var pageIndex: Int = 0
     var weatherViewModel = WeatherViewModel()
 
+    override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey : Any]? = nil) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
