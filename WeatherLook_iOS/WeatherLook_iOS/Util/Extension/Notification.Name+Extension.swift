2296d34820de6dadbd9635a38ef25837d93195c9
전소영 <jsu3417@gmail.com>
2022-03-12 04:08:41 +0900

    [#13] Notification.Name+Extension 추가

---
+++ WeatherLook_iOS/WeatherLook_iOS/Util/Extension/Notification.Name+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.Name+Extension.swift
+//  WeatherLook_iOS
+//
+//  Created by 전소영 on 2022/03/12.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let addLocation = NSNotification.Name(rawValue: "addLocation")
+}
