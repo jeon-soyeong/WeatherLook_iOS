e779f6ad2180c52e2c89b6a1228ebfacaeba0730
전소영 <jsu3417@gmail.com>
2022-05-04 23:06:02 +0900

    [#17] APIService decode 로직 변경

--- WeatherLook_iOS/WeatherLook_iOS/Network/APIService.swift
+++ WeatherLook_iOS/WeatherLook_iOS/Network/APIService.swift
@@ -12,15 +12,15 @@ import RxSwift
 
 class APIService {
     static let shared = APIService()
-
-    func request<T: Codable, API: TargetType>(_ target: API) -> Single<T> {
+    
+    func request<T: Codable, API: TargetType>(_ target: API, _ responseType: T.Type) -> Single<T> {
         return Single<T>.create { single in
             let provider = MoyaProvider<API>(session: DefaultSession.sharedSession)
             let request = provider.request(target) { result in
                 switch result {
                 case .success(let response):
                     do {
-                        let parsedResponse = try JSONDecoder().decode(T.self, from: response.data)
+                        let parsedResponse = try JSONDecoder().decode(responseType, from: response.data)
                         single(.success(parsedResponse))
                     } catch let error {
                         single(.failure(error))
@@ -33,38 +33,4 @@ class APIService {
             return Disposables.create { request.cancel() }
         }
     }
-    
-    func testRequest<C: Codable, T: TargetType>(target: T, responseType: C.Type, completion: @escaping (Result<C, APIError>) -> Void) {
-        let provider = MoyaProvider<T>(session: DefaultSession.sharedSession)
-        
-        provider.request(target) { result in
-            switch result {
-            case .success(let response):
-                if 400..<500 ~= response.statusCode {
-                    completion(.failure(.clientError))
-                    print("clientError")
-                } else if 500..<600 ~= response.statusCode {
-                    completion(.failure(.serverError))
-                    print("serverError")
-                }
-                do {
-                    let parsedResponse = try JSONDecoder().decode(responseType, from: response.data)
-                    completion(.success(parsedResponse))
-                } catch {
-                    print(error.localizedDescription)
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-                completion(.failure(.networkError))
-                print("networkError")
-            }
-        }
-    }
-}
-
-
-enum APIError: Error {
-    case clientError
-    case serverError
-    case networkError
 }
