ec51ac4ffecc5c742b5bf22050fac7d172a605d4
전소영 <jsu3417@gmail.com>
2022-06-08 00:35:34 +0900

    [#18] deleteButton, stickerButton, arrowButton 추가

--- WeatherLook_iOS/WeatherLook_iOS/Source/Share/View/PreviewViewController.swift
+++ WeatherLook_iOS/WeatherLook_iOS/Source/Share/View/PreviewViewController.swift
@@ -11,6 +11,18 @@ class PreviewViewController: UIViewController {
     weak var coordinator: PreViewCoordinator?
     var imageView = UIImageView()
     
+    private let deleteButton = UIButton().then {
+        $0.setImage(UIImage(named: "delete"), for: .normal)
+    }
+    
+    private let stickerButton = UIButton().then {
+        $0.setImage(UIImage(named: "sticker"), for: .normal)
+    }
+    
+    private let arrowButton = UIButton().then {
+        $0.setImage(UIImage(named: "arrow"), for: .normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
@@ -23,11 +35,32 @@ class PreviewViewController: UIViewController {
     
     private func setupSubviews() {
         view.addSubview(imageView)
+        view.addSubview(deleteButton)
+        view.addSubview(stickerButton)
+        view.addSubview(arrowButton)
     }
     
     private func setupConstraints() {
         imageView.snp.makeConstraints {
             $0.centerX.centerY.width.height.equalToSuperview()
+        }
+        
+        deleteButton.snp.makeConstraints {
+            $0.top.equalTo(54)
+            $0.leading.equalToSuperview().inset(24)
+            $0.width.height.equalTo(18)
+        }
+        
+        stickerButton.snp.makeConstraints {
+            $0.top.equalTo(50)
+            $0.trailing.equalToSuperview().inset(60)
+            $0.width.height.equalTo(28)
+        }
+        
+        arrowButton.snp.makeConstraints {
+            $0.top.equalTo(54)
+            $0.trailing.equalToSuperview().inset(22)
+            $0.width.height.equalTo(22)
         }
     }
 }
