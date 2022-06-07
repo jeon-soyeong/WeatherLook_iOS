ad22418a98b55fb01715a6db38d6d38f63023ee7
전소영 <jsu3417@gmail.com>
2022-06-07 23:33:42 +0900

    [#18] PreviewViewController setupView 추가

--- WeatherLook_iOS/WeatherLook_iOS/Source/Share/View/PreviewViewController.swift
+++ WeatherLook_iOS/WeatherLook_iOS/Source/Share/View/PreviewViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class PreviewViewController: UIViewController {
     weak var coordinator: PreViewCoordinator?
+    var imageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
     }
-
+    
+    private func setupView() {
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    private func setupSubviews() {
+        view.addSubview(imageView)
+    }
+    
+    private func setupConstraints() {
+        imageView.snp.makeConstraints {
+            $0.centerX.centerY.width.height.equalToSuperview()
+        }
+    }
 }
