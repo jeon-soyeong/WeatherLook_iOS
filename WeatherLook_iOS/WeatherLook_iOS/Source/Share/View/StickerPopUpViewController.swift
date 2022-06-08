4723cb891df7f25503896ba72b1560688cc4d2c6
전소영 <jsu3417@gmail.com>
2022-06-08 16:18:59 +0900

    [#18] StickerPopUpViewController backgroundView 추가

--- WeatherLook_iOS/WeatherLook_iOS/Source/Share/View/StickerPopUpViewController.swift
+++ WeatherLook_iOS/WeatherLook_iOS/Source/Share/View/StickerPopUpViewController.swift
@@ -13,8 +13,27 @@ class StickerPopUpViewController: UIViewController {
     weak var coordinator: StickerPopUpCoordinator?
     private let disposeBag = DisposeBag()
     
+    private let backgroundView = UIView().then {
+        $0.backgroundColor = .transparentGrey
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
+    }
+    
+    private func setupView() {
+        setupSubViews()
+        setupConstraints()
+    }
+    
+    private func setupSubViews() {
+        view.addSubview(backgroundView)
+    }
+    
+    private func setupConstraints() {
+        backgroundView.snp.makeConstraints {
+            $0.centerX.centerY.width.height.equalToSuperview()
+        }
     }
 }
