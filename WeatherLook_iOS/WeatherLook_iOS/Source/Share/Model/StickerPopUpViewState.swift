bab8682298e4c29fe124c5efbae51fdfe2921476
전소영 <jsu3417@gmail.com>
2022-06-09 17:54:10 +0900

    [#18] StickerPopUpViewState enum 추가

---
+++ WeatherLook_iOS/WeatherLook_iOS/Source/Share/Model/StickerPopUpViewState.swift
@@ -0,0 +1,13 @@
+//
+//  StickerPopUpViewState.swift
+//  WeatherLook_iOS
+//
+//  Created by 전소영 on 2022/06/09.
+//
+
+import Foundation
+
+enum StickerPopUpViewState {
+    case expanded
+    case normal
+}
