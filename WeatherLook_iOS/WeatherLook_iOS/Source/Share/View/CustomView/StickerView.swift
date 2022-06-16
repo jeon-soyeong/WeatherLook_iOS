d08fe0486d59a3de156f7a5b0784fac08be29333
전소영 <jsu3417@gmail.com>
2022-06-16 23:29:36 +0900

    [#18] pan, pinch, rotation gesture 추가

--- WeatherLook_iOS/WeatherLook_iOS/Source/Share/View/CustomView/StickerView.swift
+++ WeatherLook_iOS/WeatherLook_iOS/Source/Share/View/CustomView/StickerView.swift
@@ -7,20 +7,28 @@
 
 import UIKit
 
+import RxSwift
+
 class StickerView: UIView {
+    private let disposeBag = DisposeBag()
+    
     var stickerImageView = UIImageView()
     private let deleteButton = UIButton().then {
         $0.setImage(UIImage(named: "stickerDelete"), for: .normal)
     }
+    private var beginningPoint = CGPoint.zero
+    private var beginningCenter = CGPoint.zero
     
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
         setupView()
+        setupGestureRecognizer()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupView()
+        setupGestureRecognizer()
     }
     
     private func setupView() {
@@ -44,5 +52,74 @@ class StickerView: UIView {
             $0.leading.equalTo(2)
             $0.width.height.equalTo(20)
         }
-    }   
+    }
+    
+    private func setupGestureRecognizer() {
+        let panGestureRecognizer = UIPanGestureRecognizer()
+        panGestureRecognizer.delaysTouchesBegan = false
+        panGestureRecognizer.delaysTouchesEnded = false
+        self.addGestureRecognizer(panGestureRecognizer)
+        
+        let pinchGestureRecognizer = UIPinchGestureRecognizer()
+        self.addGestureRecognizer(pinchGestureRecognizer)
+        
+        let rotationGestureRecognizer = UIRotationGestureRecognizer()
+        self.addGestureRecognizer(rotationGestureRecognizer)
+        
+        panGestureRecognizer.rx.event
+            .subscribe(onNext: { [weak self] _ in
+                self?.handlePanGesture(panGestureRecognizer)
+            })
+            .disposed(by: disposeBag)
+        
+        pinchGestureRecognizer.rx.event
+            .subscribe(onNext: { [weak self] _ in
+                self?.handlePinchGesture(pinchGestureRecognizer)
+            })
+            .disposed(by: disposeBag)
+        
+        rotationGestureRecognizer.rx.event
+            .subscribe(onNext: { [weak self] _ in
+                self?.handleRotationGesture(rotationGestureRecognizer)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func handlePanGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
+        let touchLocation = gestureRecognizer.location(in: self.superview)
+        switch gestureRecognizer.state {
+        case .began:
+            self.beginningPoint = touchLocation
+            self.beginningCenter = self.center
+        case .changed:
+            self.center = CGPoint(x: self.beginningCenter.x + (touchLocation.x - self.beginningPoint.x), y: self.beginningCenter.y + (touchLocation.y - self.beginningPoint.y))
+        case .ended:
+            self.center = CGPoint(x: self.beginningCenter.x + (touchLocation.x - self.beginningPoint.x), y: self.beginningCenter.y + (touchLocation.y - self.beginningPoint.y))
+        default:
+            break
+        }
+    }
+    
+    private func handlePinchGesture(_ gestureRecognizer: UIPinchGestureRecognizer) {
+        guard let gestureView = gestureRecognizer.view else {
+            return
+        }
+        
+        gestureView.transform = gestureView.transform.scaledBy(
+            x: gestureRecognizer.scale,
+            y: gestureRecognizer.scale
+        )
+        gestureRecognizer.scale = 1
+    }
+    
+    private func handleRotationGesture(_ gestureRecognizer: UIRotationGestureRecognizer) {
+        guard let gestureView = gestureRecognizer.view else {
+            return
+        }
+        
+        gestureView.transform = gestureView.transform.rotated(
+            by: gestureRecognizer.rotation
+        )
+        gestureRecognizer.rotation = 0
+    }
 }
