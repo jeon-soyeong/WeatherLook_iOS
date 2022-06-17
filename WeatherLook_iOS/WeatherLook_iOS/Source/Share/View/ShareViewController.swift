7996e6580481e323c2651e259cd522948add1a53
전소영 <jsu3417@gmail.com>
2022-06-17 21:33:25 +0900

    [#18] ShareViewController 추가

---
+++ WeatherLook_iOS/WeatherLook_iOS/Source/Share/View/ShareViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ShareViewController.swift
+//  WeatherLook_iOS
+//
+//  Created by 전소영 on 2022/06/17.
+//
+
+import UIKit
+
+import RxSwift
+
+class ShareViewController: UIViewController {
+    weak var coordinator: ShareCoordinator?
+    private let disposeBag = DisposeBag()
+    
+    var capturedShareImage: UIImage?
+    
+    let backgroundImageView = UIImageView()
+    
+    private let shareButton = UIButton().then {
+        $0.setImage(UIImage(named: "shareButton"), for: .normal)
+    }
+    
+    private let backButton = UIButton().then {
+        $0.setImage(UIImage(named: "backArrow"), for: .normal)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        bindAction()
+    }
+    
+    private func setupView() {
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    private func setupSubviews() {
+        view.addSubview(backgroundImageView)
+        view.addSubview(shareButton)
+        view.addSubview(backButton)
+    }
+    
+    private func setupConstraints() {
+        backgroundImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.top.equalTo(52)
+            $0.leading.equalTo(24)
+            $0.width.height.equalTo(24)
+        }
+        
+        shareButton.snp.makeConstraints {
+            $0.top.equalTo(50)
+            $0.trailing.equalToSuperview().inset(18)
+            $0.width.equalTo(75)
+            $0.height.equalTo(30)
+        }
+    }
+    
+    private func bindAction() {
+        shareButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                
+            })
+            .disposed(by: disposeBag)
+        
+        backButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.popShareViewController()
+            })
+            .disposed(by: disposeBag)
+    }
+}
